0d445ce2b437d17395564f47e67e79f6ce83a2a6
jn <jose.a.nunez@gmail.com>
2024-03-10 20:47:25 -0600

    jn-file-list-copy logic

--- copy.js
+++ copy.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env node
 
-console.log("the copier will go here");
+const csv = require('csv-parser');
+const fs = require('fs-extra');
+const path = require('path');
+
+const [,, csvFilePath, maxAgeDays, targetPath] = process.argv;
+
+if (!csvFilePath || !maxAgeDays || !targetPath) {
+  console.log('Usage: node copy.js <path-to-file-list.csv> <max-age-days> <target-path>');
+  process.exit(1);
+}
+
+async function copyFiles(csvFilePath, maxAgeDays, targetPath) {
+  const maxAgeDate = new Date();
+  maxAgeDate.setDate(maxAgeDate.getDate() - maxAgeDays);
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on('data', (row) => {
+      const { PATH: filePath, 'LAST ACCESS DATE': accessDate } = row;
+      const fileAccessDate = new Date(accessDate);
+
+      if (fileAccessDate >= maxAgeDate) {
+        const relativePath = path.relative(path.dirname(csvFilePath), filePath);
+        const targetFilePath = path.join(targetPath, relativePath);
+
+        fs.ensureDir(path.dirname(targetFilePath))
+          .then(() => fs.copy(filePath, targetFilePath))
+          .then(() => console.log(`Copied: ${filePath} -> ${targetFilePath}`))
+          .catch(err => console.error(`Error copying file ${filePath}:`, err));
+      }
+    })
+    .on('end', () => {
+      console.log('Finished processing CSV.');
+    });
+}
+
+copyFiles(csvFilePath, maxAgeDays, targetPath).catch(console.error);
